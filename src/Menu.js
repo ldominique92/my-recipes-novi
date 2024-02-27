5c4928b530aeab247680fb4d13d76ffb583aed00
Larissa Dominique Garcia <larissadominique92@gmail.com>
2024-02-27 23:36:39 +0100

    Login using api

--- src/Menu.js
+++ src/Menu.js
@@ -115,13 +115,9 @@ async function loginAction({ request }) {
     };
   }
 
-  // Sign in and redirect to the proper destination if successful.
   try {
-    await noviAuthProvider.signin(username);
+    await noviAuthProvider.signin(username, password);
   } catch (error) {
-    // Unused as of now but this is how you would handle invalid
-    // username/password combinations - just like validating the inputs
-    // above
     return {
       error: "Invalid login attempt",
     };
--- src/auth.js
+++ src/auth.js
@@ -1,14 +1,31 @@
-  export const noviAuthProvider = {
-    isAuthenticated: false,
-    username: null,
-    async signin(username) {
-      await new Promise((r) => setTimeout(r, 500)); // fake delay
-      noviAuthProvider.isAuthenticated = true;
-      noviAuthProvider.username = username;
-    },
-    async signout() {
-      await new Promise((r) => setTimeout(r, 500)); // fake delay
-      noviAuthProvider.isAuthenticated = false;
-      noviAuthProvider.username = "";
-    },
-  };
+import axios from 'axios';
+
+const appUrl = 'https://api.datavortex.nl/testapp';
+
+export const noviAuthProvider = {
+  isAuthenticated: false,
+  username: null,
+  token: null,
+  async signin(username, password) {
+    await axios({
+      method: 'post',
+      url: appUrl + '/users/authenticate',
+      data: {
+        username: username,
+        password: password
+      }
+    })
+    .then((response) =>
+    {
+      if (response.status == 200 && response.data && response.data.jwt) {
+        noviAuthProvider.token = response.data.jwt;
+        noviAuthProvider.isAuthenticated = true;
+        noviAuthProvider.username = username;
+      }
+    });
+  },
+  async signout() {
+    noviAuthProvider.isAuthenticated = false;
+    noviAuthProvider.username = "";
+  },
+};
